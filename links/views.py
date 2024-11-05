9cfb3c7e7e028df63fece7fe7b8eb310df850f50
Omi Gonzalez <omigonzalezamengual@gmail.com>
2024-11-05 13:25:10 +0100

    modificado templates y views

--- links/views.py
+++ links/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Link
 
 # Create your views here.
 def link_list(request):
-    return render(request, 'links/link_list.html', {})
+    links = Link.objects.filter(created_date__lte=timezone.now())
+    return render(request, 'links/link_list.html', {'links': links})
