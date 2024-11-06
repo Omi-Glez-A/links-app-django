8b539e9e3d70d13c36880a19d4ba79a74f0d4f6d
Omi Gonzalez <omigonzalezamengual@gmail.com>
2024-11-06 11:12:32 +0100

    modified views, added static directory, base.html, bulma css

--- links/views.py
+++ links/views.py
@@ -5,4 +5,4 @@ from .models import Link
 # Create your views here.
 def link_list(request):
     links = Link.objects.filter(created_date__lte=timezone.now())
-    return render(request, 'links/link_list.html', {'links': links})
+    return render(request, 'links/link_list.html', {'links': links, 'title': 'Home'})
